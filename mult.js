0171d58184e6bb77d97048c1af20c5f213751c25
Nathaniel oyinbo <programmingwithnathbenwhite@gmail.com>
2023-08-17 17:37:36 +0100

    publish to github

--- mult.js
+++ mult.js
@@ -9,32 +9,34 @@ const formEl = document.getElementById("form");
 const inputEl = document.getElementById("input");
 
 const scoreEl = document.getElementsById("score");
- if(!score) {
-    score = 0;
- }
 
- scoreEl.innerText = `score: ${score}`;
- 
- questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
 
 let score = JSON.parse(localStorage.getItem("score"));
 
+if(!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+
+
 const correctAns = num1 * num2;
 
-formEl.addEventListener("submit", (e) => {
-  e.preventDefault();
-
+formEl.addEventListener("submit", () => {
   const userAns = +inputEl.value;
   if (userAns === correctAns) {
     score++;
-    updateLocalStorage();
+    updateLocalStorage()
   } else {
     score--;
-    updateLocalStorage();
+    updateLocalStorage()
   }
 
 });
-function updateLocalStorage() {
+function updateLocalStorage(){
   localStorage.setItem("score", JSON.stringify(score));
 
 
